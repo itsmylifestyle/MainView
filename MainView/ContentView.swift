b71c156b8be58f93065675f63fff083e15199dd7
itsmylifestyle <hahaii00@mail.ru>
2022-12-25 16:03:58 +0600

    Initial Commit

---
+++ MainView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MainView
+//
+//  Created by Айбек on 25.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MainView/MainViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewApp.swift
+//  MainView
+//
+//  Created by Айбек on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MainViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
